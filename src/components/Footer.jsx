19b6ac152f579051984e4dceaa4ca13257bd26ef
Teja Janga <tejat7927@gmail.com>
2026-05-04 05:36:33 +0530

    Updated few changes in Footer.jsx file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import { BsGithub } from "react-icons/bs";
 
 const Footer = () => (
-    <footer className="mt-auto py-6 border-t bg-white/60 border-slate-400 flex flex-col gap-2 items-center px-2">
-        <p className="text-black/75 text-[12px] font-medium">
-            © {new Date().getFullYear()} | Developed by <span className="text-blue-500 hover:text-blue-600 cursor-pointer font-bold">Teja Janga - テジャ</span>
+    <footer className="mt-auto py-4 border-t bg-white/60 border-slate-500 flex flex-col gap-3 items-center px-2">
+        <p className="text-black/75 text-sm font-bold">
+            © {new Date().getFullYear()} | Developed by <span className="text-[#213577] cursor-pointer font-bold">Teja Janga - テジャ</span>
         </p>
         <a 
             href="https://github.com/Teja-Janga/SOLUM" 
@@ -11,7 +11,7 @@ const Footer = () => (
             rel="noreferrer"
             className="flex items-center gap-1 text-black/70 hover:text-black transition-colors"
         >
-            <span className="text-[12px] font-medium uppercase tracking-widest">View Source</span>
+            <span className="text-[12px] font-bold uppercase tracking-widest">View Source</span>
             <BsGithub size={20} />
         </a>
     </footer>
